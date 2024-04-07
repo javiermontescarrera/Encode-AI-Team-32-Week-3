5c75ab6bb74194322c3ca3327b66b164dd90492a
Javier Montes <javier.montes.carrera@gmail.com>
2024-04-07 02:56:22 +0000

    feat: First version of painting designer and painting creation solution

---
+++ app/api/chat/route.ts
@@ -0,0 +1,53 @@
+import OpenAI from 'openai/index.mjs';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  baseURL: "http://127.0.0.1:5000/v1",
+});
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+ 
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages: [
+        {
+          role: 'system',
+          content: 'You are a creative art painting expert with a proficient knowledge of every style of painting who loves designing paintings of any style you are asked to. But you don{t give any details of the style itself',
+        },
+        {
+          role: 'system',
+          content: 'You will be provided with the name of a style of painting',
+        },
+        {
+          role: 'system',
+          content: 'Based on style you were provided, you will generate a title for a painting of that style',
+        },
+        
+        {
+          role: 'system',
+          content: 'After you have generated the title of the painting, you will generate everything needed for a it based on the provided painting style: a title, its dimensions and you also a detailed description of the painting that you designed.',
+        },
+        {
+          role: 'system',
+          content: 'You are forbiden to explain anything about the style.',
+        },
+        {
+          role: 'system',
+          content: 'You will only answer with the painting style, the titel of the painting and the description of the painting that you have designed.',
+        },
+        ...messages,
+      ],
+  });
+ 
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
---
+++ app/page.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useChat } from "ai/react";
+import { useEffect, useRef, useState } from "react";
+
+export default function Chat() {
+  const {
+    messages,
+    isLoading,
+    append,
+  } = useChat();
+  const styles = [
+    { emoji: "🖼️", value: "Realism" },
+    { emoji: "🖼️", value: "Painterly" },
+    { emoji: "🖼️", value: "Impressionism" },
+    { emoji: "🖼️", value: "Expressionism" },
+    { emoji: "🖼️", value: "Fauvism" },
+    { emoji: "🖼️", value: "Photorealism" },
+    { emoji: "🖼️", value: "Cubism" },
+  ];
+
+  const [state, setState] = useState({
+    style: "",
+  }); 
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const [imageIsLoading, setImageIsLoading] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      messagesContainerRef.current.scrollTop =
+        messagesContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  if (imageIsLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loader">
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-700 h-10 w-10"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (image) {
+    return (
+      <div className="card w-full h-screen max-w-md py-24 mx-auto stretch">
+        <img src={`data:image/jpeg;base64,${image}`} />
+        <textarea
+          className="mt-4 w-full text-white bg-black h-64"
+          value={messages[messages.length - 1].content}
+          readOnly
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col w-full h-screen max-w-md py-24 mx-auto stretch">
+      <div className="overflow-auto mb-8 w-full" ref={messagesContainerRef}>
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={`whitespace-pre-wrap ${
+              m.role === "user"
+                ? "bg-green-700 p-3 m-2 rounded-lg"
+                : "bg-slate-700 p-3 m-2 rounded-lg"
+            }`}
+          >
+            {m.role === "user" ? "User: " : "AI: "}
+            {m.content}
+          </div>
+        ))}
+
+        {isLoading && (
+          <div className="flex justify-end pr-4">
+            <span className="animate-bounce">...</span>
+          </div>
+        )}
+      </div>
+      <div className="fixed bottom-0 w-full max-w-md">
+        <div className="flex flex-col justify-center mb-2 items-center">
+          {messages.length == 0 && (
+            <div>
+              <div className="space-y-4 bg-opacity-25 bg-gray-700 rounded-lg p-4">
+                <h3 className="text-xl font-semibold">Styles</h3>
+
+                <div className="flex flex-wrap justify-center">
+                  {styles.map(({ value, emoji }) => (
+                    <div
+                      key={value}
+                      className="p-4 m-2 bg-opacity-25 bg-gray-600 rounded-lg"
+                    >
+                      <input
+                        id={value}
+                        type="radio"
+                        name="style"
+                        value={value}
+                        onChange={handleChange}
+                      />
+                      <label className="ml-2" htmlFor={value}>
+                        {`${emoji} ${value}`}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <button
+                className="bg-blue-500 p-2 text-white rounded shadow-xl"
+                disabled={isLoading || !state.style}
+                onClick={() =>
+                  append({ role: "user", content: state.style })
+                }
+              >
+                Design a painting
+              </button>
+            </div>
+          )}
+          {messages.length == 2 && !isLoading && (
+            <button
+              className="bg-blue-500 p-2 text-white rounded shadow-xl"
+              disabled={isLoading}
+              onClick={async () => {
+                setImageIsLoading(true);
+                const response = await fetch("api/images", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    message: messages[messages.length - 1].content,
+                  }),
+                });
+                const data = await response.json();
+                setImage(data);
+                setImageIsLoading(false);
+              }}
+            >
+              Generate painting
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
